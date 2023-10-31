9dc1b8390a1000ebabb192c2c00ea948ad0423d7
Catarina Guimarães <110750750+CatarinaGuima@users.noreply.github.com>
2023-10-30 22:24:49 -0300

    Reserva adicionada

--- client/src/Pages/profile.js
+++ client/src/Pages/profile.js
@@ -1,205 +1,189 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity, Alert, Image } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-
-const handleSave = () => {
-  Alert.alert('Reserva Concluída!');
-};
+import {useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, FlatList,  } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export function ProfileScreen() {
 
-  const [fullName, setFullName] = useState('');
-  const [email, setEmail] = useState('');
-  const [city, setCity] = useState('');
+  const [nome, setNome] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [numeroMesa, setNumeroMesa] = useState('');
+  const [numeroCliente, setNumeroCliente] = useState('');
+  const [data, setData] = useState('');
+  const [horario, setHorario] = useState('');
+  const [reservas, setReservas] = useState([]);
 
+  function handleAddReserva() {
+    const reserva = { nome, telefone, numeroMesa, numeroCliente, data, horario };
+    setReservas([...reservas, reserva]);
+    setNome('');
+    setTelefone('');
+    setNumeroMesa('');
+    setNumeroCliente('');
+    setData('');
+    setHorario('');
+  }
+
+  function handleDeleteReserva(index) {
+    const newReservas = [...reservas];
+    newReservas.splice(index, 1);
+    setReservas(newReservas);
+  }
+
+  function handleEditReserva(index) {
+    const reserva = reservas[index];
+    setNome(reserva.nome);
+    setTelefone(reserva.telefone);
+    setNumeroMesa(reserva.numeroMesa);
+    setNumeroCliente(reserva.numeroCliente)
+    setData(reserva.data);
+    setHorario(reserva.horario);
+    handleDeleteReserva(index);
+  }
 
   return (
     <View style={styles.container}>
-      <View style={styles.profileContainer}>
-        <View style={styles.addPhotoContainer}>
-          <Text style={styles.addPhotoText}>Crie uma nova reserva</Text>
-        </View>
-        <View style={styles.infoContainer}>
-          <View style={styles.formField}>
-            <Icon name="user" size={20} style={styles.infoIcon} />
-            <View style={styles.inputContainer}>
-              <TextInput
-                style={styles.infoTextInput}
-                multiline
-                placeholder="Nome Completo"
-                onChangeText={text => setFullName(text)}
-              />
-            </View>
-          </View>
-          <View style={styles.formField}>
-            <Icon name="phone" size={20} style={styles.infoIcon} />
-            <View style={[styles.inputContainer]}>
-              <TextInput
-                style={styles.infoTextInput}
-                multiline
-                placeholder="(__)_____-____"
-                onChangeText={text => setEmail(text)}
-              />
-            </View>
-          </View>
-          <View style={styles.formField}>
-            <TextInput
-              style={styles.infobox}
-              keyboardType='numeric'
-              placeholder="Qtd de Pessoas"
-              onChangeText={text => setCity(text)}
-            />
-          </View>
-          <View style={styles.formField}>
-            <TextInput
-              style={styles.infobox}
-              keyboardType='numeric'
-              placeholder="Mesa"
-              onChangeText={text => setCity(text)}
-            />
-          </View>
-          <View style={styles.formField}>
-            <Text style={styles.title}>Horário:</Text>
-            <TextInput
-              style={styles.infobox}
-              keyboardType='numeric'
-              placeholder="__:__"
-              onChangeText={text => setCity(text)}
-            />
-          </View>
-          <View style={styles.formField}>
-            <Text style={styles.title}>Data:</Text>
-            <TextInput
-              style={styles.infobox}
-              keyboardType='numeric'
-              placeholder="DD/MM/AAAA"
-              onChangeText={text => setCity(text)}
-            />
-          </View>
-          <View style={styles.formField}>
-            <Text style={styles.title}>Observações:</Text>
-            <TextInput
-              style={[styles.infobox, styles.obs]}
-              multiline
-              keyboardType='default'
-              placeholder=""
-              onChangeText={text => setCity(text)}
-            />
-          </View>
-        </View>
-        <TouchableOpacity style={styles.saveButton} onPress={handleSave}>
-          <Text style={styles.saveButtonText}>Confirmar Reserva</Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>The Black Angus</Text>
+        <Text style={styles.subtitle}>Reservas de Mesas</Text>
+      </View>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome"
+          value={nome}
+          onChangeText={setNome}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Telefone"
+          value={telefone}
+          onChangeText={setTelefone}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Número da Mesa"
+          value={numeroMesa}
+          onChangeText={setNumeroMesa}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Número de Clientes"
+          value={numeroCliente}
+          onChangeText={setNumeroCliente}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Data"
+          value={data}
+          onChangeText={setData}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Horário"
+          value={horario}
+          onChangeText={setHorario}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleAddReserva}
+        >
+          <Text style={styles.buttonText}>Adicionar Reserva</Text>
         </TouchableOpacity>
       </View>
-    </View >
+      <FlatList
+        data={reservas}
+        keyExtractor={(_, index) => String(index)}
+        renderItem={({ item, index }) => (
+          <View style={styles.reservaContainer}>
+            <Text style={styles.reservaText}>{item.nome} - {item.telefone} - Mesa {item.numeroMesa} - {item.numeroCliente} Pessoas- {item.data} - {item.horario}</Text>
+            <TouchableOpacity
+              style={styles.reservaButton}
+              onPress={() => handleEditReserva(index)}
+            >
+              <Ionicons
+                name="create-outline"
+                size={24}
+                color="#FFF"
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.reservaButton}
+              onPress={() => handleDeleteReserva(index)}
+            >
+              <Ionicons
+                name="trash-outline"
+                size={24}
+                color="#FFF"
+              />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFF', //cor do plano de fundo geral
-    paddingHorizontal: 20,
-    paddingTop: 50
+    backgroundColor: '#FFF',
+    padding: 20,
   },
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 20
-  },
-  titleText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginLeft: 10
-  },
-  profileContainer: {
-    alignItems: 'center'
-  },
-  circleContainer: {
-    borderWidth: 3,
-    borderColor: '#000',
-    borderRadius: 100,
+  header: {
+    backgroundColor: 'black',
     padding: 10,
-    marginBottom: 20
-  },
-  circleIcon: {
-    color: '#000'
-  },
-  addPhotoContainer: {
-    alignItems: 'center',
-    marginBottom: 20
-  },
-  addPhotoButton: {
-    flexDirection: 'row',
-    alignItems: 'center'
-  },
-  addPhotoIcon: {
-    marginRight: 10
-  },
-  addPhotoText: {
-    fontSize: 24,
-    color: '#000'
-  },
-  infoContainer: {
-    alignItems: 'flex-start'
-  },
-  formField: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 10
-  },
-  infoIcon: {
-    marginRight: 10
-  },
-  inputContainer: {
-    flex: 1,
-    borderWidth: 1,
-    borderColor: '#000',
-    borderRadius: 10,
-    backgroundColor: '#FFF', // Cor mais clara de dentro do container
-    marginRight: 10
-  },
-  infoTextInput: {
-    fontSize: 18,
-    color: '#000',
-    paddingHorizontal: 10
-  },
-  infobox: {
-    fontSize: 18,
-    height: 50,
-    color: '#000',
-    paddingHorizontal: 10,
-    borderWidth: 1,
+    marginBottom: 10,
   },
   title: {
-    fontSize: 18,
-    fontWeight: 'bold'
-  },
-  obs: {
-   width: 260,
-   height: 210
-  },
-  saveButton: {
-    backgroundColor: '#000',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 10,
-    marginTop: 20
-  },
-  saveButtonText: {
+    fontSize: 20,
+    fontWeight: 'bold',
     color: '#FFF',
-    fontSize: 18
+    textAlign: 'center',
   },
-  image: {
-    display: 'flex',
-    borderRadius: 50,
+  subtitle: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: '#FFF',
+    textAlign: 'center',
+  },
+  form: {
     marginBottom: 20,
-    alignItems: 'flex-end',
-    marginLeft: 20,
-    width: 150,
-    height: 150,
-    borderWidth: 5,
-    borderColor: '#E6E6FA',
   },
-});
+  input: {
+    height: 40,
+    borderColor: '#DDD',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  button: {
+    backgroundColor: 'black',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+  },
+  reservaContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+    backgroundColor: '#DDD',
+    padding: 10,
+    borderRadius: 5,
+  },
+  reservaText: {
+    flex: 1,
+    marginRight: 10,
+  },
+  reservaButton: {
+    backgroundColor: 'black',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginLeft: 10,
+  }
+})
